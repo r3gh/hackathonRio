316ce3caf95fba527e86323860e40ed1889f05b8
Gustavo Daniel <gustavodsf1@gmail.com>
2017-12-02 15:53:40 -0200

    save on database

--- ott-crawler/api/src/postgre.py
+++ ott-crawler/api/src/postgre.py
@@ -15,28 +15,55 @@ class Postgres:
   def insertOndeFoiRoubado(self, json):
     cur = self.conn.cursor()
     hashmap = {}
-    print(json[0])
+
     for idx in range(len(json)):
+      strData = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+      if json[idx]['created_at'] != None:
+        idxEnd = json[idx]['created_at'].find('.')
+        json[idx]['created_at'] = json[idx]['created_at'][:idxEnd]
+        date = datetime.strptime(json[idx]['created_at'], '%Y-%m-%dT%H:%M:%S');
+        strData = date.strftime('%Y-%m-%d %H:%M:%S')
+      json[idx]['created_at'] = strData
       hashmap[json[idx]['created_at']]=json[idx]
 
     for k,violence in hashmap.items():
-      strData = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-      if violence['created_at'] != None:
-        idxEnd = violence['created_at'].find('.')
-        violence['created_at'] = violence['created_at'][:idxEnd]
-        date = datetime.strptime(violence['created_at'], '%Y-%m-%dT%H:%M:%S');
-        strData = date.strftime('%Y-%m-%d %H:%M:%S')
-      violence['titulo'] = ( violence['titulo'] if violence['titulo'] != None  else 'Não Informado')
-      queryStr = "INSERT INTO violence_data(title, latitude, longitude, event_data) VALUES ('"\
-+violence['titulo']+"',"+str(violence['latitude'])+","+str(violence['longitude'])+",'"+ strData +"')"
+      violence['titulo']  = (violence['titulo'] if violence['titulo'] != None  else 'Não Informado')
+      violence['registrou_bo']  = (str(violence['registrou_bo']) if str(violence['registrou_bo']) != None  else 'Não Informado')
+      violence['valor_prejuizo']  = (violence['valor_prejuizo'] if violence['valor_prejuizo'] != None  else 'Não Informado')
+      violence['bairro']  = (violence['bairro'] if violence['bairro'] != None  else 'Não Informado')
+      violence['nome']  = (violence['nome'] if violence['nome'] != None  else 'Não Informado')
+      violence['tipo_assalto_id']  = (str(violence['tipo_assalto_id']) if str(violence['tipo_assalto_id']) != None  else 'Não Informado')
+      violence['descricao']  = (violence['descricao'] if violence['descricao'] != None  else 'Não Informado')
+      violence['sexo']  = (str(violence['sexo']) if str(violence['sexo']) != None  else 'Não Informado')
+      violence['endereco']  = (violence['endereco'] if violence['endereco'] != None  else 'Não Informado')
+      violence['municipio_id']  = (str(violence['municipio_id']) if str(violence['municipio_id']) != None  else 'Não Informado')
+      
+      queryStr = "INSERT INTO violence_data(title, latitude, longitude, event_data, bulletin_occurrence,damage_value, neighborhood, county,name,type_violence,description,sex,address,source) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
       try:
-        cur.execute(queryStr)
+        cur.execute(queryStr,(violence['titulo'],str(violence['latitude']),str(violence['longitude']),violence['created_at'], violence['registrou_bo'],violence['valor_prejuizo'],violence['bairro'],violence['municipio_id'],violence['nome'],violence['tipo_assalto_id'],violence['descricao'],str(violence['sexo']),violence['endereco'],'ONDE_FOI_ROUBADO'))
         self.conn.commit()
-      except:
-        print ("Erro ao inserir")
+      except psycopg2.Error as e:
+        a = 1;
     cur.close()
-   
-    
+
+  def insertOndeTemTiro(self, json):
+    cur = self.conn.cursor()
+    for violence in json['rows']:
+      descriptionArray = violence[0].split("#")
+      strData = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+      if descriptionArray[1] != None:
+        date = datetime.strptime(descriptionArray[1], '%d/%m/%Y às %H:%M');
+        strData = date.strftime('%Y-%m-%d %H:%M:%S')
+
+      queryStr = "INSERT INTO violence_data(title, latitude, longitude, event_data, bulletin_occurrence,damage_value, neighborhood, county,name,type_violence,description,sex,address,source) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+      try:
+        cur.execute(queryStr,(descriptionArray[3],str(violence[4]),str(violence[5]),strData, 'Não Informado',0,violence[1].split(',')[1],'Não Informado',violence[2],0,descriptionArray[3],'Não Informado',violence[1],'ONDE_TEM_TIRO'))
+        self.conn.commit()
+      except psycopg2.Error as e:
+        print(e)
+    cur.close()
+     
+      
   def close(self):
     try:
       self.conn.close()
@@ -44,32 +71,3 @@ class Postgres:
       print ("I am unable to connect to the database")
 
 
-
-
-      {'hashid': 'VKEmM9',
-       'registrou_bo': None, 
-       'data': '2017-11-29', 
-       'uri': '/denuncias/VKEmM9-jovens-da-roda-de-rima-quarta-feira', 
-       'valor_prejuizo': '10.0', 
-       'votos': 0, 
-       'bairro': 'Todos os Santos', 
-       'email': '', 'hora': '2000-01-01T21:30:32.000Z', 
-       'gmaps': None, 
-       'titulo': 
-       'Jovens da roda de rima (Quarta feira)', 
-       'municipio_id': 3654, 
-       'nome': None, 
-       'ip_address_origin': None, 
-       'aasm_state': 'active', 
-       'id': 86488, 
-       'longitude': -43.2836724479575, 
-       'tipo_assalto_id': 10, 
-       'latitude': -22.8979315656398, 
-       'updated_at': '2017-11-30T11:44:26.575-02:00', 
-       'descricao': 'Segundo a vítimas, um grupo elementos de pele escura saíram de um carro prata, posteriormente roubado, onde elementos  realizaram o arrastão, além dos jovens que estavam na praça bebendo e conversando, moradores que passavam pelo local também foram abordados e quem observada de longe conseguiram correr dos assaltantes entrando em condomínios nas proximidades.', 
-       'created_at': '2017-11-30T11:44:26.575-02:00', 
-       'sexo': 1, 
-       'ativo': False, 
-       'data_hora_registro': None, 
-       'user_id': None, 
-       'endereco': 'Rua Silva Rabelo, 156, Todos os Santos'}
