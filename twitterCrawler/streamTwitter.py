5963d48aa347a159157b3c6c172cdcf5e649bcc3
hugdiniz <hugdiniz@gmail.com>
2017-12-02 15:00:55 -0200

    creating tokens config

--- twitterCrawler/streamTwitter.py
+++ twitterCrawler/streamTwitter.py
@@ -4,7 +4,7 @@ from tweepy.streaming import StreamListener
 import numpy as np
 import pickle
 import json
-
+  
 tokenFile = open("tokens", "r")
 tokensString = (tokenFile.read())
 tokens = json.loads(tokensString )
@@ -13,14 +13,8 @@ consumer_key = tokens['consumer_key']
 consumer_secret = tokens['consumer_secret']
 access_token = tokens['access_token']
 access_secret = tokens['access_secret']
-  
+ 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
  
 api = tweepy.API(auth)
-
-fnames = ['AlertaAssaltoRJ','alertario24hrs','UNIDOSPORJPA']
-for fname in fnames:
-    stuff = api.user_timeline(screen_name = fname, count = 2000, include_rts = True)
-    with open(fname,'wb') as f:
-        pickle.dump(stuff, f)
