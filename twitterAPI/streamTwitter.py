6ffe744c8bba337e3f68af9ef14b736212a2a26e
hugdiniz <hugdiniz@gmail.com>
2017-12-02 16:45:09 -0200

    factorization...

--- twitterAPI/streamTwitter.py
+++ twitterAPI/streamTwitter.py
@@ -2,8 +2,9 @@ import tweepy
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import numpy as np
-import pickle
 import json
+import numpy as np
+from functions import *
 
 tokenFile = open("tokens", "r")
 tokensString = (tokenFile.read())
@@ -13,14 +14,15 @@ consumer_key = tokens['consumer_key']
 consumer_secret = tokens['consumer_secret']
 access_token = tokens['access_token']
 access_secret = tokens['access_secret']
-  
+ 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
  
 api = tweepy.API(auth)
 
-fnames = ['AlertaAssaltoRJ','alertario24hrs','UNIDOSPORJPA']
-for fname in fnames:
-    stuff = api.user_timeline(screen_name = fname, count = 2000, include_rts = True)
-    with open(fname,'wb') as f:
-        pickle.dump(stuff, f)
+stream_listener = StreamTwitter()
+stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+print("Initializing ...")
+stream.filter(track=twitterTrackings)
+
+            
