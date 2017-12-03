828f22b3a9e164696eaaca8c2cc3f2e694dcee3d
raulsenaferreira <raulsenaferreira@gmail.com>
2017-12-03 03:50:13 -0200

    machine learning

--- client/cgi-bin/machine_learning.py
+++ client/cgi-bin/machine_learning.py
@@ -1,42 +1,91 @@
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomForestClassifier
+#!/usr/bin/env python
+import sys
+import json
+import urllib.request
+import psycopg2
+#from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_regression
 from sklearn.metrics import accuracy_score
 import numpy as np
 from sklearn import preprocessing
 from sklearn import svm
+import cgi, cgitb
+
+cgitb.enable()  # debug
+
+data = cgi.FieldStorage()
 
 #test
-text = np.genfromtxt('modified_result.csv', delimiter="\t", dtype=None, skip_header=1)
-arrData = []
+#text = np.genfromtxt('modified_result.csv', delimiter="\t", dtype=None, skip_header=1)
+r = urllib.request.urlopen("http://10.20.3.181:8080/violence/get/3000")
+arrData = json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
+#print(text)
+#arrData = []
+'''
 for t in text:
 	arrData.append(str(t).split(","))
 arrData = np.array(arrData)
-
-regr = RandomForestClassifier(max_depth=2, random_state=0)
+'''
+#regr = RandomForestClassifier(max_depth=2, random_state=0)
 clf = svm.SVC()
-#le = preprocessing.LabelEncoder()
-
-indices = np.where(arrData[:,2] == '2')[0] #crime type = 2
+le = preprocessing.LabelEncoder()
+#print(arrData[0])
+#indices = np.where(arrData["type"] == '2')[0] #crime type = 2
 #print(indices)
-data = arrData[indices, 2:3]
-labels = arrData[indices, 0]
-qtyTrain = len(indices)*0.8
-trainData = data[:qtyTrain]
+'''
+result = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0,'10':0,'11':0,'12':0,'13':0,'None':0}
+for k in arrData:
+	result[k["type"]]+=1
+print(result)
+'''
+
+data = []
+labels = []
+for a in arrData:
+	if a["type"] == '2':
+		data.append([a["neighborhood"],a["day_of_week"]])
+		labels.append(a["shift"])
+
+
+qtyTrain = int(len(labels)*0.8)
+data = np.array(data)
+
+attr1 = data[:,0]
+le.fit(attr1)
+attr1Train = le.transform(attr1[:qtyTrain]) 
+attr1Test = le.transform(attr1[qtyTrain:])
+
+attr2 = data[:,1]
+le.fit(attr2)
+attr2Train = le.transform(attr2[:qtyTrain])
+attr2Test = le.transform(attr2[qtyTrain:])
+
+testData = []
+trainData = []
+
+for i in range(len(attr1Train)):
+	trainData.append([attr1Train[i], attr2Train[i]])
+for x in range(len(attr1Test)):
+	testData.append([attr1Test[x], attr2Test[x]])
+
+labels = np.array(labels)
 trainLabels = labels[:qtyTrain]
-#le.fit(trainLabels)
-regr.fit(trainData, trainLabels)
+
+#print(trainData)
+#print(testData)
+
+#regr.fit(trainData, trainLabels)
 clf.fit(trainData, trainLabels)
-testData = data[qtyTrain:]
+
 testLabels = labels[qtyTrain:]
 #le.fit(testLabels)
 #testLabels = le.transform(testLabels)
 #X, y = make_regression(n_features=4, n_informative=2, random_state=0, shuffle=False)
 #print(regr.feature_importances_)
-predicted = regr.predict(testData)
+#predicted = regr.predict(testData)
 #print(testLabels)
 #print(predicted)
-print(accuracy_score(testLabels, predicted))
+#print(accuracy_score(testLabels, predicted))
 
 predicted = clf.predict(testData)
 #print(testLabels)
--- client/source.php
+++ client/source.php
@@ -1,5 +1,13 @@
 <?php
-	exec('python3.4 cgi-bin/kde.py', $dataFromPython);
-	#echo($dataFromPython);
-	echo(empty($dataFromPython)) ? "python não carregou" : json_encode($dataFromPython);
+	$type = trim($_POST['type']);
+	if($type=='kde'){
+		exec('python3.4 cgi-bin/kde.py', $dataFromPython);
+		//echo($dataFromPython);
+		echo(empty($dataFromPython)) ? "python não carregou" : json_encode($dataFromPython);
+	}
+	else{
+		exec('python3.4 cgi-bin/machine_learning.py', $dataFromPython);
+		//echo($dataFromPython);
+		echo(empty($dataFromPython)) ? "python não carregou" : json_encode($dataFromPython);	
+	}
 ?>
